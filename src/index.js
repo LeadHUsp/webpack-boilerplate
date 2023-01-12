6218583c1c8fe3371bc3324ca37c447395e35c95
Богдан Винокуров <bodvinurk@gmail.com>
2023-01-12 14:26:49 +0300

    remove lib

--- src/index.js
+++ src/index.js
@@ -10,14 +10,11 @@ import 'swiper/scss';
 import { Modal } from './blocks/modal/modal';
 
 // Appending to the DOM
-import AirDatepicker from 'air-datepicker';
-import 'air-datepicker/air-datepicker.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Drawer();
   new Modal();
   initReactApp();
-  new AirDatepicker('#my-element');
 });
 initVueApp();
 // if (document.querySelector('header')) {
